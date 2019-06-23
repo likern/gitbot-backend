0e24fd6d7b32279891d09256a8bc81577a89f4b3
Malov Victor <malvikus@gmail.com>
2019-06-23 14:42:16 +0300

    Add authentication for version 1

--- gitbot/auth/auth.py
+++ gitbot/auth/auth.py
@@ -1,7 +1,10 @@
-from sanic.exceptions import Unauthorized
-
+from sanic import Blueprint, response
+from sanic.exceptions import Unauthorized, ServerError
+from gitbot.exceptions import UnprocessableEntity
 from firebase_admin import auth
 from firebase_admin.auth import AuthError
+
+db = None
 
 async def verify_token(request):
   try:
@@ -23,3 +26,21 @@ async def verify_token(request):
       message="Either JWT is invalid, or App’s project ID cannot be determined",
       scheme="Bearer"
     )
+
+blueprint = Blueprint('auth')
+@blueprint.route("/signup", methods=['POST'])
+async def signup(request):
+    user_id = request["user_id"]
+    user = await db.users.find_one(
+        filter={
+            "_id": user_id
+        }
+    )
+
+    if user is not None:
+      raise UnprocessableEntity("User is already registered")
+
+    res = await db.users.insert_one({"_id": user_id })
+    if not res.acknowledged or res.inserted_id != user_id:
+        raise ServerError("Failed to singup a new user")
+    return response.json({}, status=200)
